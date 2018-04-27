079bcacf7475a4de37fc96009ee6a59ed6261d3e
maduri.vassaramo@hotmail.co.uk <maduri.vassaramo@hotmail.co.uk>
2018-04-27 10:26:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // write your code below!
 function happyHolidays() {
-  return "Happy Holidays!"
+  return "Happy holidays!"
 }
-function happyHolidayTo(name) {
+function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday, name) {
